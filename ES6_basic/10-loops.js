aa02f7ba039b20b2534653ea525184df00e06328
Groinkb <benjamin@mossu.ch>
2024-01-24 19:04:02 +0100

    add

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+export default function appendToEachArrayValue(array, appendString) {
+    let index = 0;
+    for (const value of array) {
+        /* eslint-disable no-param-reassign */
+        array[index] = appendString + value;
+        index += 1;
+    }
+
+    return array;
+}
