7d8f9ba344ef9f94772481f84432eaffdfabf7e3
Groinkb <benjamin@mossu.ch>
2024-05-31 14:26:27 +0200

    add rdme and first task of the basic python code

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,1 +1,29 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" class BasicCache
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ class BasicCache that
+    inherits from BaseCaching
+    and is a caching system:
+    """
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+
+        if key is None or key not in self.cache_data:
+            return None
+
+        return self.cache_data[key]
