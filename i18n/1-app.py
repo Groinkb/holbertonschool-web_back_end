0d94403a660bd7e21d5f4ca1a1738d0c6c4c4ea2
Groinkb <benjamin@mossu.ch>
2024-07-05 12:50:03 +0200

    add the second attempt to finish the second task

---
+++ i18n/1-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+from flask import request
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def output_index():
+    """
+    route that simply outputs “Welcome to Holberton” as page title (<title>)
+    and “Hello world” as header (<h1>).
+    """
+    return render_template('0-index.html')
+
+
+class Config:
+    """
+    Config class for flask app
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
